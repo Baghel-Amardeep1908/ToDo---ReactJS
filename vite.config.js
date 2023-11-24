ecb58078962dca9a2d599be610b4963a8faaae80
Baghel-Amardeep1908 <baghel.amar1908@gmail.com>
2023-11-25 04:40:15 +0530

    Fixed Base URL in Vite Config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/Baghel-Amardeep1908/ToDo---ReactJS.git",
-
+  base: "https://baghel-amardeep1908.github.io/ToDo---ReactJS/",
   plugins: [react()],
 });
